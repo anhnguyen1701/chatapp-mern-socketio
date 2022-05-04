1baf93bb0340cd295d6d408dd0d54e0c719df883
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-04 22:22:00 +0700

    fetch data

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Chatpage = () => {
-  return (
-    <div>Chatpage</div>
-  )
-}
+    const [chats, setChats] = useState([]);
 
-export default Chatpage
+    const fetchChats = async () => {
+        const { data } = await axios.get('/api/chat');
+        setChats(data);
+    };
+
+    useEffect(() => {
+        fetchChats();
+    }, []);
+
+    return (
+        <div>
+            {chats.map((chat) => (
+                <div key={chat._id}>{chat.chatName}</div>
+            ))}
+        </div>
+    );
+};
+
+export default Chatpage;
