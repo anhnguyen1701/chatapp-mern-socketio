d57c0a5b067ed99280e269ecb516974e3ef1c8ec
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-04 21:37:11 +0700

    add chakraui & proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
       hello
+      <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
