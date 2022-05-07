ae4cd01af43e03c711c4682641ec795d40539997
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-08 01:01:05 +0700

    send message api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 dotenv.config();
@@ -13,11 +14,12 @@ const app = express();
 app.use(express.json()); // accept JSON data
 
 app.get('/', (req, res) => {
-    res.send('API is running');
+  res.send('API is running');
 });
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+app.use('/api/message', messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
