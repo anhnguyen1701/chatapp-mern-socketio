803b2b35c959ede66776c624a65f4ada75545672
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-24 10:55:54 +0700

    make it better

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -17,7 +17,7 @@ import axios from 'axios';
 import ScrollableChat from './ScrollableChat';
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'https://mern-chatapp-socketio.herokuapp.com/';
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
