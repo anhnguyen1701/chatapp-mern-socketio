46c5ff0bd7fe6a2038398e30f98feddba75124a0
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-24 11:27:18 +0700

    commit message

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,6 @@ app.use('/api/message', messageRoutes);
 // ---deploy---
 
 const __dirname1 = path.resolve("/react/chatapp-mern-socketio");
-console.log(__dirname1);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname1, '/frontend/build')));
 
