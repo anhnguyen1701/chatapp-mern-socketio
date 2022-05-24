72a923cd39372daa06b441112b09ca8600112903
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-24 10:37:49 +0700

    setup deploy

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const path = require('path');
 
 dotenv.config();
 connectDB();
@@ -13,13 +14,27 @@ const app = express();
 
 app.use(express.json()); // accept JSON data
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
+
+// ---deploy---
+
+const __dirname1 = path.resolve("/react/chatapp-mern-socketio");
+console.log(__dirname1);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
+
+// ---deploy---
 
 app.use(notFound);
 app.use(errorHandler);
