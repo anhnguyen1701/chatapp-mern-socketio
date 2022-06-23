ad1b490a88ea9ca0a7cd30a90551015d2ae0258d
anh nguyen <nguyenlamanh1701@gmail.com>
2022-06-23 22:43:06 +0700

    fix route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,15 @@ import { Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import Chatpage from './Pages/Chatpage';
 import Home from './Pages/NewApp/home/Home';
+import Profile from './Pages/NewApp/profile/Profile';
 
 function App() {
   return (
     <>
       <Route path="/home" component={Home} />
-      <div className="App">
-        <Route path="/" component={Homepage} exact />
-        <Route path="/chats" component={Chatpage} />
-      </div>
+      <Route path="/profile/:username" component={Profile}></Route>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
     </>
   );
 }
