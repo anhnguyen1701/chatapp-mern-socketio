105ee0491cbb9de38c0266c597995362c4761c8e
anh nguyen <nguyenlamanh1701@gmail.com>
2022-06-26 01:02:35 +0700

    remove some stuff right bar

--- frontend/src/components/NewApp/rightbar/Rightbar.jsx
+++ frontend/src/components/NewApp/rightbar/Rightbar.jsx
@@ -85,28 +85,7 @@ export default function Rightbar({ user }) {
             {followed ? <Remove /> : <Add />}
           </button>
         )}
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">{user.city}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">{user.from}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">
-              {user.relationship === 1
-                ? 'Single'
-                : user.relationship === 1
-                ? 'Married'
-                : '-'}
-            </span>
-          </div>
-        </div>
-        <h4 className="rightbarTitle">User friends</h4>
+        <h4 className="rightbarTitle">Following</h4>
         <div className="rightbarFollowings">
           {friends.map((friend) => (
             <Link
